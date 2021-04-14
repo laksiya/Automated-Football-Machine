6dcdbabe3759e29cc1bb16ddc34c8a8860391e72
Laksiya Balasubramaniam <36662731+laksiya@users.noreply.github.com>
2021-04-14 11:23:20 +0200

    post form

--- flaskapp.py
+++ flaskapp.py
@@ -2,7 +2,8 @@
 # https://asdkazmi.medium.com/deploying-flask-app-with-wsgi-and-apache-server-on-ubuntu-20-04-396607e0e40f
 # To get access to html folder: sudo chown (check out raspberrys how to apache site)
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+ 
 from datetime import datetime
 
 app = Flask(__name__)
@@ -20,6 +21,14 @@ def cakes(name):
 def cakes_default():
     time = datetime.now()
     return render_template('cakes.html', name="", time=time)
+
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
